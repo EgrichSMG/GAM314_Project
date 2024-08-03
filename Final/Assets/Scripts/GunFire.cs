f267494b4b7002acf2e5d72fb957c6bad736b7a0
EgrichSMG <rfcole2003@gmail.com>
2024-08-02 18:47:42 -0700

    Shooting Updated

---
+++ Final/Assets/Scripts/GunFire.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunFire : MonoBehaviour
+{
+    [SerializeField] private LayerMask RandomCubesLayer;
+
+    private Camera BillyCamera;
+
+    private void Start()
+    {
+        BillyCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray gunray = new Ray(BillyCamera.transform.position, BillyCamera.transform.forward);
+            if (Physics.Raycast(gunray, out RaycastHit hitinfo, 100f, RandomCubesLayer))
+            {
+                Destroy(hitinfo.collider.gameObject);
+            }
+        }
+    }
+}
